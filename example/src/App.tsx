d8889ad35fb1a6b3763eddbff7bd5f5d387d2485
Yousof Abouhalawa <work@yousofabouhalawa.com>
2026-01-19 10:16:48 +0200

    chore: fix typos, change scope, update yarn.lock

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,7 +9,7 @@ import {
   ScrollView,
 } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { Alert } from '@yousofabouhalawa/react-native-alert';
+import { Alert } from '@nasaq/react-native-alert';
 import { Card } from './components/Card';
 
 export default function App() {
